53e4887369fb16a51fc5d978f56d73d1b34d8b0e
Stefan Hartmann <stefan.hartmann@leica-geosystems.com>
2020-10-28 11:16:51 +0100

    init

---
+++ MyLibrary/MyLibrary/MyLibrary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class MyLibrary
+    {
+        public void SayHelloWorld()
+        {
+            Console.Out.WriteLine("Hello World");
+        }
+    }
+}
